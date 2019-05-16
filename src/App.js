ed68600295b2b1729b387a83eab920db582210ca
Campalo <25295489+Campalo@users.noreply.github.com>
2019-05-16 14:02:02 +0200

    test if the dev branch is protected

--- src/App.js
+++ src/App.js
@@ -7,15 +7,12 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Test</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           Learn React
         </a>
       </header>
