ca9852d123fcc16f650d39d7fe5206b87f4c3613
jenna <jenna_kim1@hotmail.com>
2019-05-16 18:12:07 +0200

    added Search component in Home

--- src/Component/Header/Search.js
+++ src/Component/Header/Search.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import data from './data.json';
-import Card from './Card'
 import { InputGroup, InputGroupAddon, InputGroupText, Input } from 'reactstrap';
 
  export default class Test extends React.Component {
