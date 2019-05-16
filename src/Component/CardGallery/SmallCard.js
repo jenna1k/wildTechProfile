987cc4b290c7c3400fee72b9ea0b75e4555faa42
unknown <mirzabagherian.asieh@gmail.com>
2019-05-16 22:34:28 +0200

    Create Small Card

--- src/Component/CardGallery/SmallCard.js
+++ src/Component/CardGallery/SmallCard.js
@@ -5,7 +5,7 @@ const SmallCard = (props) => {
   return (
     <div>
       <Card>
-        <CardImg top width="100%" src="https://media.licdn.com/dms/image/C4D03AQH7JYoNXLQoCg/profile-displayphoto-shrink_200_200/0?e=1563408000&v=beta&t=PcOZXYVuZXzFEEr0Silb009SnjgHoPXD52WSNa47ez4" />
+        <CardImg top width="100%" src="" />
         <CardBody>
           <CardTitle style={{color: 'black'}} >Name</CardTitle>
           <CardSubtitle style={{color: 'black'}}>favorite programming languages</CardSubtitle>
