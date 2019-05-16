cf2d6238cb4d6c0362442cca29e8094e848a030a
Campalo <25295489+Campalo@users.noreply.github.com>
2019-05-17 00:09:33 +0200

    created first version header

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import './Header.css';
 
 function Header() {
   return (
-    <header>
-      <img src="https://i.imgur.com/emFe3O8.jpg" alt="WildCodeSchool logo" />
-      <div>search bar</div>
-      <div>
-        <p>location filter</p>
-        <p>programming languages filter</p>
+    <header className="wcs_header">
+      <div className="wcs_header_items">
+        <img src="https://i.imgur.com/emFe3O8.jpg" alt="WildCodeSchool logo" />
+        <span>search bar</span>
+      </div>
+      <div className="wcs_header_items">
+        <span>location filter</span>
+        <span>programming languages filter</span>
       </div>
     </header>
   );
