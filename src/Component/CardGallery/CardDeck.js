56933efe490cde1fba65f2aba0d053110c81ff00
hiijs <bernadetta.nycz@icloud.com>
2019-05-16 20:31:56 +0200

    layout update

--- src/Component/CardGallery/CardDeck.js
+++ src/Component/CardGallery/CardDeck.js
@@ -1,32 +1,40 @@
 import React from "react";
 import "./CardDeck.css";
-import {
-  Col,
-  Row,
-  Container
-} from "reactstrap";
+import { Col, Row, Container } from "reactstrap";
 
-const CardDeck =props => {
-    return (
-        <Container id="profilesprev">
-  
-        
-        <Row>
-                  <Col xs="12" md="4" lg="4"> text </Col>
-                  <Col xs="12" md="4" lg="4"> text </Col>
-                  <Col xs="12" md="4" lg="4"> text </Col>
-        </Row>
-        <Row>
-                  <Col xs="12" md="4" lg="4"> text </Col>
-                  <Col xs="12" md="4" lg="4"> text </Col>
-                  <Col xs="12" md="4" lg="4"> text </Col>
-        </Row>
-
-
-
-        </Container>
-    );
+const CardDeck = props => {
+  return (
+    <Container id="profilesprev">
+      <Row>
+        <Col xs="12" md="4" lg="4">
+          {" "}
+          text{" "}
+        </Col>
+        <Col xs="12" md="4" lg="4">
+          {" "}
+          text{" "}
+        </Col>
+        <Col xs="12" md="4" lg="4">
+          {" "}
+          text{" "}
+        </Col>
+      </Row>
+      <Row>
+        <Col xs="12" md="4" lg="4">
+          {" "}
+          text{" "}
+        </Col>
+        <Col xs="12" md="4" lg="4">
+          {" "}
+          text{" "}
+        </Col>
+        <Col xs="12" md="4" lg="4">
+          {" "}
+          text{" "}
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default CardDeck;
-
