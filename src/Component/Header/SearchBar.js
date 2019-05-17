0479e3fa0bb19c16bf13b5aa3571cff0b4b19d9a
Campalo <25295489+Campalo@users.noreply.github.com>
2019-05-17 02:29:43 +0200

    update header to be responsive + sticky + search bar from Jenna

--- src/Component/Header/SearchBar.js
+++ src/Component/Header/SearchBar.js
@@ -5,7 +5,12 @@ const SearchBar = ({ search, searchClick, searchChange }) => {
   return (
     <div>
       {/* <Input type="search" placeholder="search" onChange={searchChange}/>
-        <Button onClick={searchClick}> Go </Button> */}
+        <Button onClick={searchClick}> Go </Button>
+        
+        JENNA, 
+        I HOPE i DID NOT BREAK ANYTHING, TELL ME TOMORROW
+        ;D
+        CAMILLE */}
 
       <InputGroup>
         <Input type="search" placeholder="search" onChange={searchChange} />
