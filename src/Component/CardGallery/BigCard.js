1884e99ca50fc8608e2b49b01f9f10944b4ba622
Raphael Lautenbacher <datingel@Raphaels-MacBook-Pro.local>
2019-05-17 12:46:42 +0200

    minor updates

--- src/Component/CardGallery/BigCard.js
+++ src/Component/CardGallery/BigCard.js
@@ -7,15 +7,41 @@ const BigCard = props => {
   return (
     <div>
       <Card className="card-container">
-        <CardImg className="avatar" src={props.basics.picture} alt={props.basics.name} />
+        <CardImg className="avatar" src={props.basics.picture} alt="" />
 
         <CardBody className="card-body-text">
           <CardText>Name:{props.basics.name}</CardText>
           <CardText>Email: {props.basics.email}</CardText>
-          <CardText>Skills: </CardText>
+
+          <div className="skills">
+            <p>Skills: </p>
+            {props.favorite_programming_languages.toString(favorite => (
+              <CardText>
+                {favorite
+                  .charAt(0)
+                  .toUpperCase()
+                  .slice(1)}
+              </CardText>
+            ))}
+          </div>
+
           <CardText>Bio: {props.basics.summary}</CardText>
-          <CardText>Interests: </CardText>
-          <CardText>Latest Project Title: Technologies:</CardText>
+
+          <div className="interests">
+            {props.interests.map(interest => (
+              <CardText>Passionate about: {interest.name}</CardText>
+            ))}
+          </div>
+
+          <div>
+            {props.projects.map(project => (
+              <CardText>Latest Project Title: {project.title}</CardText>
+            ))}
+            {props.projects.technologies.toString(technology => (
+              <CardText>Used Tech Stack: {technology}</CardText>
+            ))}
+          </div>
+
           <div className="icons">
             {props.basics.profiles.map(profile => (
               <SocialIcon url={profile.url} />
