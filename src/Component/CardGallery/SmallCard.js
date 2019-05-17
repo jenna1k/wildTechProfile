b7fe1b6687925e7772dc06ebb1601c0b3f40289e
unknown <mirzabagherian.asieh@gmail.com>
2019-05-17 12:07:13 +0200

    icons replaced with buttons

--- src/Component/CardGallery/SmallCard.js
+++ src/Component/CardGallery/SmallCard.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Button } from 'reactstrap';
+import ReactDOM from 'react-dom';
+import { SocialIcon } from 'react-social-icons';
+import MdMoreHoriz from 'react-icons/lib/md/more-horiz';
 
 const SmallCard = (props) => {
   return (
@@ -7,10 +10,15 @@ const SmallCard = (props) => {
       <Card style={{width:"10%", height:"40%"}}>
         <CardImg top style={{width:"70%", position:"relative"}} src={props.basics.picture} alt={props.basics.name}/>
         <CardBody>
-          <CardTitle style={{color:'black', textalign:"justify", textsize:"5%"}} ><em>{props.basics.name}</em></CardTitle>
-          <CardSubtitle style={{color:'black', textalign:"justify",textsize:"5%"}}>{props.favorite_programming_languages}</CardSubtitle>
-          <Button target="_blank" href={props.basics.email}>Contact</Button>
-          <Button target="_blank" onClick={props.handleClick}> Further </Button>
+          <CardTitle style={{color:'black', textalign:"justify", textsize:"2%"}} ><em>{props.basics.name}</em></CardTitle>
+          <CardSubtitle style={{color:'black', textalign:"justify", textsize:"2%"}}>{props.favorite_programming_languages}</CardSubtitle>
+          <div>
+            {props.basics.profiles.map(profile => (
+              <SocialIcon url={profile.url} />
+            ))}
+            <MdMoreHoriz target="_blank" onClick={props.handleClick} color="#cccccc" size={20} />
+          </div>
+
         </CardBody>
       </Card>
     </div>
