a98108ed05ba8d954867f08c806aebb34d4230d6
unknown <mirzabagherian.asieh@gmail.com>
2019-05-17 14:19:41 +0200

    with style

--- src/Component/CardGallery/SmallCard.js
+++ src/Component/CardGallery/SmallCard.js
@@ -3,15 +3,16 @@ import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Button } fr
 import ReactDOM from 'react-dom';
 import { SocialIcon } from 'react-social-icons';
 import MdMoreHoriz from 'react-icons/lib/md/more-horiz';
+import 'SmallCard.css';
 
 const SmallCard = (props) => {
   return (
     <div>
-      <Card style={{width:"10%", height:"40%"}}>
-        <CardImg top style={{width:"70%", position:"relative"}} src={props.basics.picture} alt={props.basics.name}/>
+      <Card className="avatar-card">
+        <CardImg top className="avatar-card-img" src={props.basics.picture} alt={props.basics.name}/>
         <CardBody>
-          <CardTitle style={{color:'black', textalign:"justify", textsize:"2%"}} ><em>{props.basics.name}</em></CardTitle>
-          <CardSubtitle style={{color:'black', textalign:"justify", textsize:"2%"}}>{props.favorite_programming_languages}</CardSubtitle>
+          <CardTitle className="avatar-title" ><em>{props.basics.name}</em></CardTitle>
+          <CardSubtitle className="avatar-subtitle">{props.favorite_programming_languages}</CardSubtitle>
           <div>
             {props.basics.profiles.map(profile => (
               <SocialIcon url={profile.url} />
